30c1f746ddbfd5a720b8e46033622ab15509295d
yuhsuan <r07222045@ntu.edu.tw>
2020-06-23 01:11:20 +0800

    add openMP, calculate_energy()

--- Final-project.cpp
+++ Final-project.cpp
@@ -12,13 +12,15 @@ using namespace std;
 // global constants
 
 const double G_scaled = 39.5 ; // Time in years, distances in AU, mass in solar masses
-double G = G_scaled;         // gravitational constant
-double dt = 1.0e-2;      // time interval
-int   N = 4 ;           // particle number
-const int gridN = 256;       // # of grid
-const int gridNk = 8;          // # of grid in Z direction
-const int T = 300;
-double L = 250;       // box size
+double G = 1;         // gravitational constant
+double dt = 0.1;    // time interval
+int   N = 2 ;           // particle number
+const int gridN = 513;  // # of grid
+const int gridNk = 9;          // # of grid in Z direction
+const int total_grids = gridNk * gridN * gridN ;
+const int T = 250;
+double Et , Es;
+double L = 120;      // box size
 double Lz = L * gridNk / gridN;       // box size
 const double PI = 3.1416;
 double dx = L / gridN;    // grid size 
@@ -44,6 +46,7 @@ void output_phi(string name)
 {
     fstream phifile;
     phifile.open(name, ios::out);
+    int total = gridNk * gridN * gridN;
     /*
     if (!file)
     {
@@ -53,11 +56,11 @@ void output_phi(string name)
     }
     */
     for (int i = 0; i < gridNk * gridN * gridN; i++) {
-        file << phi[i] << "\n";
+        printf("Output: %d / %d\n", i, total_grids);
+        phifile << phi[i] << "\n";
     }
 }
-
-void compute_rho(int N, int gridN , double dx, double* rho) {
+void compute_rho(int N, int gridN , double* rho) {
     // Zero-out rho for reuse
     memset(rho, 0, gridNk * gridN * gridN * sizeof(double));
     float rx, ry, rz; //the distance between the center of the grid and the particle
@@ -71,12 +74,9 @@ void compute_rho(int N, int gridN , double dx, double* rho) {
             if (p != N - 1) { file << x[p] << "," << y[p] << "," << z[p] <<","; }
             else { file << x[p] << "," << y[p] << "," << z[p] << "\n"; }
         }
-        #pragma omp parallel num_threads(3)
-        {
             i = (x[p] - (-L / 2)) / dx;
             j = (y[p] - (-L / 2)) / dx;
             k = (z[p] - (-Lz / 2)) / dx;
-        }
         //#pragma omp critical
         for (int a = i - 1;a < i + 2;a++)
             for (int b = j - 1;b < j + 2;b++)
@@ -141,17 +141,14 @@ void compute_phi_k(int gridN, double L, fftw_complex* rho_k, double G) {
     //#pragma omp for
     for (int k = 0; k < gridNk ; k++) {
         n_k = (k >= gridNk / 2) ? (gridNk-k) : k;
-        //n_k = k;
         kz_k = n_k * 2 * PI / Lz;
         for (int j = 0; j < gridN; j++) {
             n_j = (j >= gridN / 2) ? (gridN-j) : j;    // Negative frequency mapping in y
             ky_j = n_j * 2  * PI / L;          // Spatial frequency in y
             for (int i = 0; i < (gridN/2+1) ; i++) {
                 n_i = i;
-                //n_i = (i >= gridN / 2) ? (gridN - i) : i;
                 if (n_i == 0 && n_j == 0 && n_k ==0) continue;
                 kx_i = n_i * 2  * PI / L;      // Spatial frequency in x
-                //k_sq = kx_i * kx_i + ky_j * ky_j + kz_k * kz_k;
                 double fac = -1.0 * (pow(kx_i, 2) + pow(ky_j, 2) + pow(kz_k, 2))/(4 * PI * G);
                 if (fabs(fac) < 1e-14)
                 {
@@ -226,30 +223,77 @@ void compute_accelerations(int gridN, double* acc_x, double* acc_y, double* acc_
                 acc_y[k * gridN * gridN + i] = (-phi[k * gridN * gridN + (gridN - 1) * gridN + i] + phi[k * gridN * gridN + gridN + i]) / scaling_factor;
                 acc_y[k * gridN * gridN + (gridN - 1) * gridN + i] = (-phi[k * gridN * gridN + (gridN - 2) * gridN + i] + phi[k * gridN * gridN + i]) / scaling_factor;
             }
+        /*
+        printf("Ax: %f\n", a_x[180085]);
+        printf("PH: %f\n", phi[180070]);
+        printf("PH: %f\n", phi[180080]);
+        printf("Ax: %f\n", a_x[180115]);
+        printf("PH: %f\n", phi[180120]);
+        printf("PH: %f\n", phi[180125]);*/
     }
 }
 void update_particle_KDK( double* a_x, double* a_y, double* a_z)
 {
     for (int p = 0; p < N; p++) {
         int i, j, k;
-        i = floor((x[p] - (-L / 2)) / dx);
-        j = floor((y[p] - (-L / 2)) / dx);
-        k = floor((z[p] - (-Lz / 2)) / dx);
-        vx[p] = a_x[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        vy[p] = a_y[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        vz[p] = a_z[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        x[p] += vx[p] * dt;
-        y[p] += vy[p] * dt;
-        z[p] += vz[p] * dt;
-        vx[p] = a_x[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        vy[p] = a_y[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        vz[p] = a_z[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
-        if (x[p] < -0.5 * L) { x[p] = fmod(x[p] + 0.5 * L, L) + 0.5 * L; }
-        if (x[p] > 0.5 * L) { x[p] = fmod(x[p] + 0.5 * L, L) - 0.5 * L; }
-        if (y[p] < -0.5 * L) { y[p] = fmod(y[p] + 0.5 * L, L) + 0.5 * L; }
-        if (y[p] > 0.5 * L) { y[p] = fmod(y[p] + 0.5 * L, L) - 0.5 * L; }
-        if (z[p] < -0.5 * Lz) { z[p] = fmod(z[p] + 0.5 * Lz, Lz) + 0.5 * Lz; }
-        if (z[p] > 0.5 * Lz) { z[p] = fmod(z[p] + 0.5 * Lz, Lz) - 0.5 * Lz; }
+        #pragma omp parallel
+        {
+            #pragma omp single
+            {
+                i = floor((x[p] - (-L / 2)) / dx);
+                j = floor((y[p] - (-L / 2)) / dx);
+                k = floor((z[p] - (-Lz / 2)) / dx);
+            }
+            #pragma omp sections
+            {
+                #pragma omp section
+                {
+                    vx[p] = a_x[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                    x[p] += vx[p] * dt;
+                    vx[p] = a_x[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                }
+                #pragma omp section
+                {
+                    vy[p] = a_y[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                    y[p] += vy[p] * dt;
+                    vy[p] = a_y[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                }
+                #pragma omp section
+                {
+                    vz[p] = a_z[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                    z[p] += vz[p] * dt;
+                    vz[p] = a_z[k * gridN * gridN + j * gridN + i] * 0.5 * dt;
+                }
+            }
+            #pragma omp barrier
+            #pragma omp sections
+            {
+                #pragma omp section
+                {
+                    if (x[p] < -0.5 * L) { x[p] = fmod(x[p] + 0.5 * L, L) + 0.5 * L; }
+                }
+                #pragma omp section
+                {
+                    if (x[p] > 0.5 * L) { x[p] = fmod(x[p] + 0.5 * L, L) - 0.5 * L; }
+                }
+                #pragma omp section
+                {
+                    if (y[p] < -0.5 * L) { y[p] = fmod(y[p] + 0.5 * L, L) + 0.5 * L; }
+                }
+                #pragma omp section
+                {
+                    if (y[p] > 0.5 * L) { y[p] = fmod(y[p] + 0.5 * L, L) - 0.5 * L; }
+                }
+                #pragma omp section
+                {
+                    if (z[p] < -0.5 * Lz) { z[p] = fmod(z[p] + 0.5 * Lz, Lz) + 0.5 * Lz; }
+                }
+                #pragma omp section
+                {
+                    if (z[p] > 0.5 * Lz) { z[p] = fmod(z[p] + 0.5 * Lz, Lz) - 0.5 * Lz; }
+                }
+            }
+        }
     }
 }
 void update_particle_DKD(double* a_x, double* a_y, double* a_z)
@@ -276,17 +320,28 @@ void update_particle_DKD(double* a_x, double* a_y, double* a_z)
         if (z[p] > 0.5 * L) z[p] = fmod(z[p] + 0.5 * L, L);
     }
 }
+void calculate_energy() {
+    double rx, ry, rz ,rr;
+    rx = fabs(x[1] - x[0]);
+    ry = fabs(y[1] - y[0]);
+    rz = fabs(z[1] - z[0]);
+    rr = sqrt(rx * rx + ry * ry + rz * rz);
+    Es = -G * m[0] * m[1] / rr;
+}
 
 
 
 int main(int argc, char* argv[])
 {
     //int argc, char *argv[]
-    m = { 40.0, 30.0 , 20.0 , 90.0 , 30.0 , 6.5};
-    x = { 0.0, 40.0, -20.0 , -0.0, 4.2 };
+    m = { 100.0 ,100.0 , 20.0 , 90.0 , 30.0 , 6.5};
+    x = { -25.0, 25.0, -20.0 , -0.0, 4.2 };
     y = { 0.0, 0.0, -0.0 , 25.0 ,0.0};
     z = { 0.0, 0.0, 0.0 , 0.0 ,0.0};
-    int num_threads = 1;
+    vy = { 2.0, -2.0, 0.0 , 0.0 ,0.0 };
+    Et = -G * m[0] * m[1] / fabs(x[1] - x[0]);
+    //printf("total E: %f\n", Et);
+    int num_threads = 4;
     omp_set_num_threads(num_threads);
     fftw_plan_with_nthreads(num_threads);
     fftw_plan rho_plan = fftw_plan_dft_r2c_3d(gridNk, gridN, gridN, rho, rho_k, FFTW_MEASURE);
@@ -295,42 +350,42 @@ int main(int argc, char* argv[])
     file.open("Data.csv", ios::out);
     double t_start = omp_get_wtime();
     
+
     for (int t = 1; t < T; t++) {
-        printf("compute_rho\n");
-        compute_rho(N, gridN, dx, rho);
-        printf("fftw_execute\n");
+        printf("Progress: %d / %d\n", t, T);
+        //printf("compute_rho\n");
+        compute_rho(N, gridN, rho);
+        //printf("fftw_execute\n");
         #pragma omp single
         fftw_execute(rho_plan);
-        printf("compute_phi_k\n");
+        //printf("compute_phi_k\n");
         compute_phi_k(gridN, L, rho_k, G);
-
         #pragma omp single
         fftw_execute(phi_plan);
-        printf("compute_accelerations\n");
+        //printf("compute_accelerations\n");
         compute_accelerations(gridN, a_x, a_y, a_z, phi);
-        printf("update_particle\n");
+        //printf("update_particle\n");
         //for (int p = 0; p < N; p++) { printf("XYZ:%f %f %f\n", x[p], y[p], z[p]); }
         update_particle_KDK(a_x, a_y, a_z);
         //for (int p = 0; p < N; p++) { printf("XYZ:%f %f %f\n", x[p], y[p], z[p]); }
+        calculate_energy();
+        double error = 100*(Es - Et) / Et;
+        //memset(phi, 0, gridNk * gridN * gridN * sizeof(double));
+        //intf("Error; %f\n", error);
+        //output_phi("HITHIT.txt");
     }
-    /*
-    for (int i = 0; i < gridNk * gridN * gridN; i++) {
-        printf("RHOK_REAL:%f\n", rho_k[i][0]);
-        printf("RHOK_IMG:%f\n", rho_k[i][1]);
-        printf("I: %d\n",i);
-    }*/
-    
     double t_end = omp_get_wtime();
     double total_time_ms = t_end - t_start;
     printf("Time: %f\n", total_time_ms);  
-
-    
-
-    fftw_destroy_plan(rho_plan);
-    fftw_destroy_plan(phi_plan);
-    
     //output_phi("HITHIT.txt");
     //printf("dfwrpkgoekir");
+    //fftw_destroy_plan(rho_plan);
+    //fftw_destroy_plan(phi_plan);
+    //free(rho);
+    //free(a_x);
+    //free(a_y);
+    //free(a_z);
+    //fftw_free(rho_k);
     return(0);
 
 }
